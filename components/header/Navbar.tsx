3e29c911862ac31ddd032aeee4f71fe2068e78cc
MainuddinMehedi <mainuddinmazumder132@gmail.com>
2025-11-17 12:42:45 +0600

    dropdown menu fixed. navbar done.

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -1,43 +1,40 @@
-import { ChevronDown, Menu, PhoneCall } from "lucide-react";
+import { PhoneCall } from "lucide-react";
 import Link from "next/link";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "../ui/dropdown-menu";
+import CategoryDropdown from "./CategoryDropdown";
 
 export default function Navbar() {
   return (
     // <div className="h-[60px] bg-gray-800 text-white flex justify-between">
-    <div className="relative left-1/2 -translate-x-1/2 w-screen h-[60px] bg-gray-800 text-white flex justify-between">
+    <div className="relative left-1/2 -translate-x-1/2 w-screen h-[60px] bg-gray-800 text-white  font-medium flex justify-between">
       <div className="w-[90%] mx-auto flex items-center justify-between h-full">
         <div className="h-full flex gap-10">
-          <DropdownMenu>
-            <DropdownMenuTrigger className="flex items-center gap-2 h-full">
-              <div className="flex items-center justify-center bg-success h-full w-16">
-                <Menu className="w-7 h-7 text-white" />
-              </div>
-              <span className="text-[16px]">All Categories</span>
-              <ChevronDown className="w-6 h-6" />
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuItem>Fresh Fruit</DropdownMenuItem>
-              <DropdownMenuItem>Vegetable</DropdownMenuItem>
-              <DropdownMenuItem>River Fish</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-          <div className="flex items-center space-x-9">
-            <Link href={"/"}>Home</Link>
-            <Link href={"/shop"}>Shop</Link>
-            <Link href={"/blog"}>Blog</Link>
-            <Link href={"/about"}>About Us</Link>
-            <Link href={"/contact"}>Contact Us</Link>
+          <CategoryDropdown />
+
+          <div className="flex items-center space-x-11">
+            <Link href={"/"} className="whitespace-nowrap">
+              Home
+            </Link>
+            <Link href={"/shop"} className="whitespace-nowrap">
+              Shop
+            </Link>
+            <Link href={"/blog"} className="whitespace-nowrap">
+              Blog
+            </Link>
+            <Link href={"/about"} className="whitespace-nowrap">
+              About Us
+            </Link>
+            <Link href={"/contact"} className="whitespace-nowrap">
+              Contact Us
+            </Link>
           </div>
         </div>
         <div className="flex items-center gap-4">
-          <PhoneCall />
-          <span>(219) 555-0114</span>
+          <PhoneCall size={28} />
+          <span className="text-[17px]">(219) 555-0114</span>
+
+          {/* isLoggedIn && <div>
+            <User size={28} />
+          </div> */}
         </div>
       </div>
     </div>
