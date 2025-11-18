793496cf0453492e0561dc0376657719912e4c4f
MainuddinMehedi <mainuddinmazumder132@gmail.com>
2025-11-18 21:13:58 +0600

    categories section completed. fetched data using api route.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
+import Categories from "@/components/module/home/Categories";
 import Features from "@/components/module/home/Features";
 import HeroSection from "@/components/module/home/hero/HeroSection";
 
 export default function Home() {
   return (
-    <div>
+    <div className="space-y-8">
       <HeroSection />
       <Features />
+      <Categories />
     </div>
   );
 }
