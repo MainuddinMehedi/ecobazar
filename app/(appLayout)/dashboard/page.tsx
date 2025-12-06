492721899db61ab0b2d0d609b0b8daf7e01961df
MainuddinMehedi <mainuddinmazumder132@gmail.com>
2025-12-06 19:11:50 +0600

    dashboard page completed

--- app/(appLayout)/dashboard/page.tsx
+++ app/(appLayout)/dashboard/page.tsx
@@ -1,17 +1,20 @@
-import BillingAddressCard from "@/components/module/dashboard/BillingAddressCard";
-import UserProfileCard from "@/components/module/dashboard/UserProfileCard";
+import BillingAddressCard from "@/components/module/dashboard/dashboard-page/BillingAddressCard";
+import RecentOrderHistory from "@/components/module/dashboard/dashboard-page/RecentOrderHistory";
+import UserProfileCard from "@/components/module/dashboard/dashboard-page/UserProfileCard";
 
 export default function DashboardPage() {
   return (
-    <div>
+    <div className="space-y-6">
       {/* user overview */}
-      <div className="flex gap-4">
+      <div className="flex gap-6">
         <UserProfileCard />
         <BillingAddressCard />
       </div>
 
       {/* order history table */}
-      <div></div>
+      <div>
+        <RecentOrderHistory />
+      </div>
     </div>
   );
 }
